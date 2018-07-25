b0bb2c2a0376a4f0b11180f52cb69e9945a48e19
Dennis <info@dennisdijkstra.nl>
2018-07-25 17:38:09 +0200

    add whitespace

--- src/Exhibition.js
+++ src/Exhibition.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+
 const Exhibition = ({ exhibition }) => {
     const { title, id } = exhibition;
 
