bf57c2a8869b3dee3501f46d5de414b63a879a9c
Dennis <info@dennisdijkstra.nl>
2018-08-05 19:41:36 +0200

    type of object filter updates on search

--- src/ExhibitionList.js
+++ src/ExhibitionList.js
@@ -13,11 +13,12 @@ class ExhibitionList extends Component {
         };
 
         this.types = [];
+        this.filtered = [];
     }
 
     async componentDidMount() {
         try {
-            this.res = await fetch('https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getObjects&access_token=dbb5dbb3ac11def3ddd372de708e9893&query=typography&year_acquired=gt1980&has_images=1&per_page=100');
+            this.res = await fetch('https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getObjects&access_token=491c2e66a84e1faf2e7e906ff6f24579&query=typography&year_acquired=gt1980&has_images=1&per_page=100');
             const results = await this.res.json();
             this.setState({
                 exhibitionObjects: results.objects.filter(result => result.images[0]),
@@ -28,37 +29,39 @@ class ExhibitionList extends Component {
     }
 
     getTypes = (items) => {
-        items.map((object) => {
+        this.types = [];
+
+        items.forEach((object) => {
             if (!this.types.includes(object.type)) {
                 this.types.push(object.type);
             }
-            return null;
         });
     }
 
     filterTitles = () => {
         const { exhibitionObjects, search } = this.state;
-
-        return exhibitionObjects.filter(object => object.title.toLowerCase().indexOf(search.toLowerCase()) !== -1);
+        this.filtered = exhibitionObjects.filter(object => object.title.toLowerCase().indexOf(search.toLowerCase()) !== -1);
+        this.getTypes(this.filtered);
     }
 
     updateSearchValue = (search) => {
         this.setState({
             search,
         });
+
+        this.filterTitles();
     }
 
     render() {
         const { search } = this.state;
-        const filteredExhibitionObjects = this.filterTitles();
-        this.getTypes(filteredExhibitionObjects);
+        this.filterTitles();
 
         return (
             <div>
                 <div className="container">
                     <Filters updateSearchValue={this.updateSearchValue} search={search} types={this.types} />
                     <div className="exhibition-list-items content">
-                        {filteredExhibitionObjects.map(exhibition => (
+                        {this.filtered.map(exhibition => (
                             <Exhibition key={exhibition.id} exhibition={exhibition} />
                         ))}
                     </div>
