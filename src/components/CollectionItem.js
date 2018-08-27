e429be1986b2be7f473f7e9867cc3e17b532c94f
Dennis <info@dennisdijkstra.nl>
2018-08-27 18:47:39 +0200

    tweak list item styling

--- src/components/CollectionItem.js
+++ src/components/CollectionItem.js
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom';
 
 
 const CollectionItem = ({ collectionItem }) => {
-    const { title, images, id } = collectionItem;
+    const { images, id, year_acquired: year } = collectionItem;
+    const titleRaw = collectionItem.title_raw || 'No title';
 
     return (
         <div className="exhibition-list-item">
             <Link to={id}>
-                <img src={images[0].b.url} className="exhibition-list-item-image" alt={title} />
-                <p className="exhibition-list-item-title">{title}</p>
+                <img src={images[0].b.url} className="exhibition-list-item-image" alt={titleRaw} />
+                { console.log(titleRaw)}
+                <p className="exhibition-list-item-title">{titleRaw}</p>
+                <p className="exhibition-list-item-year">{year}</p>
             </Link>
         </div>
     );
@@ -20,6 +23,6 @@ export default CollectionItem;
 
 CollectionItem.propTypes = {
     collectionItem: PropTypes.shape({
-        title: PropTypes.string.isRequired,
+        titleRaw: PropTypes.string,
     }).isRequired,
 };
