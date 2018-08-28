508039909720a67736205d003d9b802bad98d653
Dennis <info@dennisdijkstra.nl>
2018-08-29 00:09:42 +0200

    storybook initial setup

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
 import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
@@ -13,7 +11,7 @@ storiesOf('Button', module)
     .add('with some emoji', () => (
         <Button onClick={action('clicked')}>
             <span role="img" aria-label="so cool">
-                😀 😎 👍 💯
+            😀 😎 👍 💯
             </span>
         </Button>
     ));
