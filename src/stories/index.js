7f60c427358d52963196ca79fc7250fb6c93c6b2
Dennis <info@dennisdijkstra.nl>
2018-08-29 17:44:57 +0200

    create some storybook stories and use some add-ons

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-import { Button, Welcome } from '@storybook/react/demo';
+import { storiesOf, addDecorator } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { Provider } from 'react-redux';
+import store from '../datamodel/store';
+import Spinner from '../components/Spinner';
+import CollectionItem from '../components/CollectionItem';
+import '../sass/app.css';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+const stories = storiesOf('Random', module);
+stories.addDecorator(withKnobs);
 
-storiesOf('Button', module)
-    .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-    .add('with some emoji', () => (
-        <Button onClick={action('clicked')}>
-            <span role="img" aria-label="so cool">
-            😀 😎 👍 💯
-            </span>
-        </Button>
-    ));
+stories
+    .addDecorator(story => <Provider store={store}>{story()}</Provider>)
+    .add('Loading spinner', () => <Spinner isLoading={boolean('isLoading', true)} />);
+
+stories
+    .addDecorator(StoryRouter())
+    .add('CollectionItem component', () => {
+        const collectionItem = {
+            id: 234,
+            images: [{
+                b: {
+                    url: 'https://images.collection.cooperhewitt.org/32762_c32801acbcb0eaba_b.jpg',
+                },
+            }],
+            title_raw: 'Titel',
+            year_end: 2002,
+        };
+        return (
+            <CollectionItem collectionItem={collectionItem} />
+        );
+    });
