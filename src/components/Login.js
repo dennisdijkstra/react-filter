420dce5312dec88b752de608432e97ff01a0107c
Dennis <info@dennisdijkstra.nl>
2018-08-31 21:14:15 +0200

    change functional component to class

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
     Field,
@@ -9,58 +9,61 @@ import {
 import RenderField from './form/RenderField';
 import validate from './form/Validate';
 
-const submit = (values, dispatch, props) => new Promise((resolve, reject) => {
-    const errors = validate(values, props);
 
-    if (Object.keys(errors).length !== 0) {
-        reject(new SubmissionError(errors));
-    } else {
-        dispatch(reset('login'));
-        resolve();
+class Login extends Component {
+    static propTypes = {
+        handleSubmit: PropTypes.func.isRequired,
+        submitting: PropTypes.bool.isRequired,
+    };
+
+    submit = (values, dispatch, props) => new Promise((resolve, reject) => {
+        const errors = validate(values, props);
+
+        if (Object.keys(errors).length !== 0) {
+            reject(new SubmissionError(errors));
+        } else {
+            dispatch(reset('login'));
+            resolve();
+        }
+    });
+
+    render() {
+        const {
+            handleSubmit,
+            submitting,
+        } = this.props;
+
+        return (
+            <div className="login content">
+                <form
+                    className="form login-form"
+                    onSubmit={handleSubmit(this.submit)}
+                >
+                    <div className="login-form-fields">
+                        <div className="form-field">
+                            <Field
+                                name="username"
+                                component={RenderField}
+                                type="text"
+                                label="Username:"
+                            />
+                        </div>
+                        <div className="form-field">
+                            <Field
+                                name="password"
+                                component={RenderField}
+                                type="password"
+                                label="Password:"
+                            />
+                        </div>
+                    </div>
+                    <button type="submit" disabled={submitting}>Login</button>
+                </form>
+            </div>
+        );
     }
-});
-
-const Login = (props) => {
-    const {
-        handleSubmit,
-        submitting,
-    } = props;
-
-    return (
-        <div className="login content">
-            <form
-                className="form login-form"
-                onSubmit={handleSubmit(submit)}
-            >
-                <div className="login-form-fields">
-                    <div className="form-field">
-                        <Field
-                            name="username"
-                            component={RenderField}
-                            type="text"
-                            label="Username:"
-                        />
-                    </div>
-                    <div className="form-field">
-                        <Field
-                            name="password"
-                            component={RenderField}
-                            type="password"
-                            label="Password:"
-                        />
-                    </div>
-                </div>
-                <button type="submit" disabled={submitting}>Login</button>
-            </form>
-        </div>
-    );
-};
+}
 
 export default reduxForm({
     form: 'login',
 })(Login);
-
-Login.propTypes = {
-    handleSubmit: PropTypes.func.isRequired,
-    submitting: PropTypes.bool.isRequired,
-};
