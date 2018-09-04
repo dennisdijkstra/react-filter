a2cd857f5a7750169975c6c266143eed93e49c5d
Dennis <info@dennisdijkstra.nl>
2018-09-04 21:37:55 +0200

    add form validation

--- src/components/form/Validate.js
+++ src/components/form/Validate.js
@@ -3,6 +3,8 @@ const validate = (values) => {
 
     if (!values.username) {
         errors.username = 'This field is required';
+    } else if (values.username.length > 15) {
+        errors.username = 'Must be 15 characters or less';
     }
 
     if (!values.password) {
